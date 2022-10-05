86126a34dbc49f71c9bcf8cb2cd273b96a6ec90c
Jordan <jordanhoward1991@gmail.com>
2022-10-05 15:10:58 -0600

    pangram in python

---
+++ pangram.py
@@ -0,0 +1,9 @@
+string = 'the quick brown fox jumps over the lazy dog'
+
+def is_pangram(str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet:
+        if char not in str.lower():
+            return False
+  
+    return True
